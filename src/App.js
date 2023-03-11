ea16dca6c4f774d3807e7aa5f02927fc0fb79892
7piyushdev <dchlamydia@gmail.com>
2023-03-11 10:23:01 +0530

    Fetch data using useEffect

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useCallback, useEffect } from "react";
+import Loading from "./components/Loading";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        // throw new Error("Something went wrong....Retrying");
+        setTimeout(() => {
+          fetchMoviesHandler();
+        }, 5000);
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <Loading />}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
